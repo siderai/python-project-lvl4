7ff54061ac7d9337590e46f74eae97ac2e4f4aa4
Alexander Sidorov <sidai@bk.ru>
2022-04-21 16:02:57 +0300

    Fix postgres

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,16 +75,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "task_manager.wsgi.application"
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
     }
 }
 
