da3e430217c4e7c61eeb8bf4258b007df2115005
Alexander Sidorov <sidai@bk.ru>
2022-04-21 15:59:11 +0300

    Add postgres as default db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,10 +75,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "task_manager.wsgi.application"
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
